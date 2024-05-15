42661eb79bdf1deb939cbad2e4ac39782c9cd733
ZaraEnti <zara.zhang@enti.cat>
2024-05-15 13:42:29 +0200

    Historial hecho
    bien guardadito + segunda parte

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,40 @@
 #include<iostream>
-#include<ofstrem>
+#include<fstream>
 #include<string>
 
 void main() {
-	ofstrem file;
-	file.open("historial.txt");
-	string mensaje = "";
-	while (mensaje != "exit") {
-		getline(cin, mensaje);
-		file << mensaje;
-		file.close();
-	}
+	//1.Historial conversation
+	//Abro archivo en modo de escritura
+	std::ofstream file_w;
+	file_w.open("historial.txt");
+	
+	std::string mensaje = "";
+	if (file_w.is_open()) {
+		while (mensaje != "exit") {
+			getline(std::cin, mensaje);
 
+			if (mensaje != "exit") {
+				file_w << mensaje << std::endl;
+			}
+		}
+		file_w.close();
+	}
+	else
+	{
+		std::cout << "Error: No se a podido abrir el archivo" << std::endl;
+	}
+	//2.Recuperar conversación
+	
+	std::ifstream file_r;
+
+	file_r.open("historial.txt");
+	
+	//Veritificación de la existencia
+	if (file_r){
+		std::cout << "¿Quieres recuperar el historial? (y/n)" << std::endl;
+		file_r.close();
+	}
+	else {
+		std::cout << "Archivo no existe" << std::endl;
+	}
 }
